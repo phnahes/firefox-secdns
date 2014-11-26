ec7c3cfd21b995dad93ca034fc994622fa9a4187
Paulo Nahes <phnahes@gmail.com>
2014-11-26 20:54:41 -0200

    Mudando logica

--- data/element-getter.js
+++ data/element-getter.js
@@ -1,3 +1,14 @@
+fh = fopen("/tmp/file.in", 0); // Open the file for reading 
+if(fh!=-1) // If the file has been successfully opened 
+{ 
+    length = flength(fh);         // Get the length of the file     
+    str = fread(fh, length);     // Read in the entire file 
+    fclose(fh);                    // Close the file 
+     
+// Display the contents of the file     
+    write(str);     
+}
+
 var keypad1 = document.createElement("div");
-keypad1.innerHTML = "Teste de DIV";
+keypad1.innerHTML = str;
 document.body.appendChild(keypad1);
