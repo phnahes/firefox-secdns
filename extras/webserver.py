3573dc0f505d08dc1cdf6e7a51e6a3ee3e8fbce5
Paulo Nahes <phnahes@gmail.com>
2014-11-27 15:22:16 -0200

    Ajustando webserver

--- extras/webserver.py
+++ extras/webserver.py
@@ -1,24 +1,61 @@
-#!/usr/bin/env python 
-import contextlib
-import SimpleHTTPServer
-import SocketServer
+#!/usr/bin/env python
+''' simple python server example; 
+    output format supported = html, raw or json '''
+import sys
+import json
 import sqlite3
-
-PORT = 8000
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 db = sqlite3.connect('/tmp/fuck.db')
 
-class DBLoggingHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+FORMATS = ('html','json','raw')
+format = FORMATS[0]
+
+html = "<html><body><H1>Python WebServer Working...</H1></body></html>"
+
+class Handler(BaseHTTPRequestHandler):
+
+    #handle GET command
     def do_GET(self):
-        db.execute("INSERT INTO crazysean (command, vers, path) VALUES (?, ?, ?)",
-                        (self.command, self.request_version, self.path))
-        db.commit()
-        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        if format == 'html':
+            self.send_response(200)
+#            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-type','text-html')
+            self.end_headers()
+#            self.wfile.write("body")
+            self.wfile.write(html)
 
-Handler = DBLoggingHandler
+	    db.execute("INSERT INTO regs (command, vers, path) VALUES (?, ?, ?)",(self.command, self.request_version, self.path))
+	    db.commit()
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+        elif format == 'json':
+            self.request.sendall(json.dumps({'path':self.path}))
+        else:
+            self.request.sendall("%s\t%s" %('path', self.path))
+        return
 
-print "serving at port", PORT
-with contextlib.closing(db):
+def run(port=8000):
+
+    print('http server is starting...')
+    #ip and port of server
+    server_address = ('127.0.0.1', port)
+    httpd = HTTPServer(server_address, Handler)
+    print('http server is running...listening on port %s' %port)
     httpd.serve_forever()
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    op = OptionParser(__doc__)
+
+    op.add_option("-p", default=8000, type="int", dest="port", 
+                  help="port #")
+    op.add_option("-f", default='json', dest="format", 
+                  help="format available %s" %str(FORMATS))
+    op.add_option("--no_filter", default=True, action='store_false', 
+                  dest="filter", help="don't filter")
+
+    opts, args = op.parse_args(sys.argv)
+
+    format = opts.format
+    run(opts.port)
+
