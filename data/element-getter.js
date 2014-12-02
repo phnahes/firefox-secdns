3e2f521d6ecabc91fb3e52ee67c0ba0c8dfba93f
Paulo Nahes <phnahes@gmail.com>
2014-12-01 23:51:37 -0200

    Testes com java script e correcoes no sniffer

--- data/element-getter.js
+++ data/element-getter.js
@@ -1,14 +1,36 @@
-fh = fopen("/tmp/file.in", 0); // Open the file for reading 
-if(fh!=-1) // If the file has been successfully opened 
-{ 
-    length = flength(fh);         // Get the length of the file     
-    str = fread(fh, length);     // Read in the entire file 
-    fclose(fh);                    // Close the file 
-     
-// Display the contents of the file     
-    write(str);     
-}
+const str = '<div id="flip">Click to slide up panel</div><div id="panel">Hello world!</div>'
+
+//const str_head_script = '$(document).ready(function(){  $("#flip").click(function(){    $("#panel").slideUp("slow");  });});'
+
+//const str_head_style = '#panel,#flip{padding:5px;text-align:center;background-color:#e5eecc;border:solid 1px #c3c3c3;}#panel{padding:50px;}' 
+
+var oScript = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
+var oScript2 = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
+var oStyle = document.createElement('style'), oHead = document.getElementsByTagName('head')[0];
+
+//var oScript = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
+//oScript.setAttribute('type', 'text/javascript');
+//oScript2.setAttribute('type', 'text/javascript');
+
+// If you want to set an external script:
+oScript.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
+oScript2.setAttribute('src', './js1.js');
+oStyle.setAttribute('src', './css1.css');
+
+// If you want to set an embedded script:
+//oScript.appendChild( document.createTextNode('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>') );
+
+
+//oScript.appendChild( document.createTextNode(str_head_style) );
+//oScript2.appendChild( document.createTextNode(str_head_script) );
+//oStyle.appendChild( document.createTextNode(str_head_style) );
+
+// Finally:
+oHead.appendChild(oScript);
+oHead.appendChild(oScript2);
+oHead.appendChild(oStyle);
 
 var keypad1 = document.createElement("div");
 keypad1.innerHTML = str;
 document.body.appendChild(keypad1);
+
--- extras/sniffer.py
+++ extras/sniffer.py
@@ -5,25 +5,31 @@ from datetime import datetime
 import time
 import datetime
 import sys
+import sqlite3
+
+db = sqlite3.connect('/tmp/fuck.db')
 
 filter_bpf = 'udp and port 53'
 
-# ------ SELECT/FILTER MSGS
 def select_DNS(pkt):
     pkt_time = pkt.sprintf('%sent.time%')
-# ------ SELECT/FILTER DNS MSGS
     try:
-        if DNSQR in pkt and pkt.dport == 53:
+        #if DNSQR in pkt and pkt.dport == 53:
         # queries
-	   print "##### DNSQR #####"
-           #print pkt.show2()
-           print pkt.sprintf("%DNS.aa%")
-	   print pkt[DNSQR].sprintf('%qname%')
+	#   print "##### DNSQR #####"
+        #   print pkt.show2()
 
-        elif DNSRR in pkt and pkt.sport == 53:
-        # responses
-	   #print "##### DNSRR #####"
-           #print pkt.show2()
+        #elif DNSRR in pkt and pkt.sport == 53:
+        if DNSRR in pkt and pkt.sport == 53:
+           #print pkt.show()
+
+	   aa = pkt.sprintf("%DNS.aa%")
+	   domain = pkt[DNSQR].sprintf('%qname%')
+
+	   if aa.find("1L") != -1:
+	   	db.execute("INSERT INTO regs (name, aa) VALUES (?, ?)",(domain, aa))
+           	db.commit()
+
            #print pkt.sprintf("%DNS.aa%")
 
     except:
