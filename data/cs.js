9c16d5557eb7e4450ad1906c6110cb91fdb211b2
Paulo Nahes <phnahes@gmail.com>
2014-12-03 20:15:45 -0200

    Quase funcionando, falta ajsutar o if da validacao

--- data/cs.js
+++
@@ -1,5 +0,0 @@
-var doc = document.location.toString().toLowerCase();
-
-self.port.on('attached', function(){
-	console.log("attached...");
-});
--- data/element-getter.js
+++ data/element-getter.js
@@ -1,32 +1,37 @@
-var doc = document.location.toString().toLowerCase();
+var url = ''
+var url = document.location.toString().toLowerCase();
 
-self.port.on('attached', function(){
-	console.log("attached..." + doc);
+self.port.on('attached', function(text) {;
 
-	const str = '<div id="mudar">'+ doc + '</div>'
+	if ( new RegExp('\\b' + text + '\\b').test(text) ) {
+		console.log("Site OK");
+	} else {
 
-	var oScript = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
-	//var oScript2 = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
-	//var oStyle = document.createElement('style'), oHead = document.getElementsByTagName('head')[0];
+		const str = '<div id="mudar">'+ text + '</div>'
 
-	//var oScript = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
-	//oScript.setAttribute('type', 'text/javascript');
-	//oScript2.setAttribute('type', 'text/javascript');
+		var oScript = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
+		//var oScript2 = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
+		//var oStyle = document.createElement('style'), oHead = document.getElementsByTagName('head')[0];
 
-	// If you want to set an external script:
-	oScript.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
+		//var oScript = document.createElement('script'), oHead = document.getElementsByTagName('head')[0];
+		//oScript.setAttribute('type', 'text/javascript');
+		//oScript2.setAttribute('type', 'text/javascript');
 
-	//oScript.appendChild( document.createTextNode(str_head_style) );
-	//oScript2.appendChild( document.createTextNode(str_head_script) );
-	//oStyle.appendChild( document.createTextNode(str_head_style) );
+		// If you want to set an external script:
+		oScript.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
 
-	// Finally:
-	oHead.appendChild(oScript);
-	//oHead.appendChild(oScript2);
-	//oHead.appendChild(oStyle);
+		//oScript.appendChild( document.createTextNode(str_head_style) );
+		//oScript2.appendChild( document.createTextNode(str_head_script) );
+		//oStyle.appendChild( document.createTextNode(str_head_style) );
 
-	var keypad1 = document.createElement("div");
-	keypad1.innerHTML = str;
-	document.body.appendChild(keypad1);
+		// Finally:
+		oHead.appendChild(oScript);
+		//oHead.appendChild(oScript2);
+		//oHead.appendChild(oStyle);
+
+		var keypad1 = document.createElement("div");
+		keypad1.innerHTML = str;
+		document.body.appendChild(keypad1);
+	}
 
 });
